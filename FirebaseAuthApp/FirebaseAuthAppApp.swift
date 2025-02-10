bc43f56e59c122395fc002748e5bab76ccf9233a
ChandanH123 <chandanhayaran@gmail.com>
2025-02-11 00:48:52 +0530

    Initial Commit

---
+++ FirebaseAuthApp/FirebaseAuthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthAppApp.swift
+//  FirebaseAuthApp
+//
+//  Created by Chandan on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
